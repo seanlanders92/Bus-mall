dcf092121ec56752adb73adc531c75225aa3b45a
seanlanders92 <seanlanders92@gmail.com>
2020-01-20 16:02:35 -0800

    displays results as a list item

--- app.js
+++ app.js
@@ -8,96 +8,98 @@ var rightImg = document.getElementById('right');
 var leftIndex = null;
 var centerIndex = null;
 var rightIndex = null;
-
+var unorderedList = document.getElementById("data");
 var Votes = 0;
 var totalRounds = 10;
 
-function Product(name, image){
-  this.name = name;
-  this.image = image;
-  this.clicked = 0;
-  this.views = 0;
+function Product(name, image) {
+    this.name = name;
+    this.image = image;
+    this.clicked = 0;
+    this.views = 0;
 
-  Product.allImages.push(this);
+    Product.allImages.push(this);
 }
 
-function randomImg(){
-  var randomNumber = Math.floor(Math.random() * Product.allImages.length);
-  return randomNumber;
+function randomImg() {
+    var randomNumber = Math.floor(Math.random() * Product.allImages.length);
+    return randomNumber;
 }
 
-function renderImg(){
- 
-  do {
-    leftIndex = randomImg();
-    centerIndex = randomImg();
-    rightIndex = randomImg();
-  } while(leftIndex === rightIndex || leftIndex === centerIndex || rightIndex === centerIndex);
+function renderImg() {
 
-  Product.allImages[leftIndex].views++;
-  Product.allImages[centerIndex].views++;
-  Product.allImages[rightIndex].views++;
+    do {
+        leftIndex = randomImg();
+        centerIndex = randomImg();
+        rightIndex = randomImg();
+    } while (leftIndex === rightIndex || leftIndex === centerIndex || rightIndex === centerIndex);
 
-  leftImg.src = Product.allImages[leftIndex].image;
-  centerImg.src = Product.allImages[centerIndex].image;
-  rightImg.src = Product.allImages[rightIndex].image;
+    Product.allImages[leftIndex].views++;
+    Product.allImages[centerIndex].views++;
+    Product.allImages[rightIndex].views++;
+
+    leftImg.src = Product.allImages[leftIndex].image;
+    centerImg.src = Product.allImages[centerIndex].image;
+    rightImg.src = Product.allImages[rightIndex].image;
 }
 
-var handleClickOnImg = function(event){
-  var productClicked = event.target.id;
+var handleClickOnImg = function (event) {
+    var productClicked = event.target.id;
 
-  if(productClicked === 'left' || productClicked === 'right' || productClicked ==='center'){
-    Votes++;
-    if(productClicked === 'left'){
-      Product.allImages[leftIndex].clicked++;
-  
-    } else if(productClicked === 'right'){
-       Goat.allImages[rightIndex].clicked++;
-      } else if(productClicked === 'center'){
-          Product.allImages[centerIndex].clicked++;
-      }
-  
-  }
-  else{
-    alert('you clicked wrong');
-  }
+    if (productClicked === 'left' || productClicked === 'right' || productClicked === 'center') {
+        Votes++;
+        if (productClicked === 'left') {
+            Product.allImages[leftIndex].clicked++;
 
-  if(Votes === totalRounds ){
+        } else if (productClicked === 'right') {
+            Product.allImages[rightIndex].clicked++;
+        } else if (productClicked === 'center') {
+            Product.allImages[centerIndex].clicked++;
+        }
 
-    stockImages.removeEventListener('click', handleClickOnImg);
-    alert('thank you for your votes');
-
-    for(var i = 0; i < Product.allImages.length; i++){
-      var product = Product.allImages[i];
-      console.log(`${product.name} received ${product.clicked} votes with ${product.views} views.`);
     }
-  }else{
-    renderImg();
-  }
+    else {
+        alert('you clicked wrong');
+    }
+
+    if (Votes === totalRounds) {
+
+        stockImages.removeEventListener('click', handleClickOnImg);
+
+        for (var i = 0; i < Product.allImages.length; i++) {
+            var datalist = document.createElement('li');
+            var product = Product.allImages[i];
+            datalist.textContent = `${product.name} received ${product.clicked} votes with ${product.views} views.`;
+            unorderedList.appendChild(datalist);
+            console.log(datalist);
+        }
+    } else {
+        renderImg();
+    }
 }
 
 Product.allImages = [];
 
-new Product('bag', '/img/bag.jpg');
-new Product('banana', '/img/banana.jpg');
-new Product('bathroom', '/img/bathroom.jpg');
-new Product('boots', '/img/boots.jpg');
-new Product('breakfast', '/img/breakfast.jpg');
-new Product('bubblegum', '/img/bubblegum.jpg');
-new Product('chair', '/img/chair.jpg');
-new Product('cthulhu', '/img/cthulhu.jpg');
-new Product('dog-duck', '/img/dog-duck.jpg');
-new Product('dragon', '/img/dragon.jpg');
-new Product('pen', '/img/pen.jpg');
-new Product('pet-sweeps', '/img/pet-sweeps.jpg');
-new Product('scissors', '/img/scissors.jpg');
-new Product('shark', '/img/shark.jpg');
-new Product('sweep', '/img/sweep.jpg');
-new Product('tauntaun', '/img/tauntaun.jpg');
-new Product('unicorn', '/img/bag.jpg');
-new Product('usb', '/img/usb.jpg');
-new Product('water-can', '/img/water-can.jpg');
-new Product('wine-glass', '/img/wine-glass.jpg');
+new Product('Bag', '/img/bag.jpg');
+new Product('Banana', '/img/banana.jpg');
+new Product('Bathroom', '/img/bathroom.jpg');
+new Product('Boots', '/img/boots.jpg');
+new Product('Breakfast', '/img/breakfast.jpg');
+new Product('Bubblegum', '/img/bubblegum.jpg');
+new Product('Chair', '/img/chair.jpg');
+new Product('Cthulhu', '/img/cthulhu.jpg');
+new Product('Dog-duck', '/img/dog-duck.jpg');
+new Product('Dragon', '/img/dragon.jpg');
+new Product('Pen', '/img/pen.jpg');
+new Product('Pet sweep', '/img/pet-sweep.jpg');
+new Product('Scissors', '/img/scissors.jpg');
+new Product('Shark', '/img/shark.jpg');
+new Product('Sweep', '/img/sweep.png');
+new Product('Tauntaun', '/img/tauntaun.jpg');
+new Product('Unicorn', '/img/bag.jpg');
+new Product('Usb', '/img/usb.gif');
+new Product('Water-can', '/img/water-can.jpg');
+new Product('Wine-glass', '/img/wine-glass.jpg');
 
 renderImg();
 
